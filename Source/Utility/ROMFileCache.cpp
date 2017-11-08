ffb7f2d541c552090ffa70cc060874631c7252d3
Paul Holden <paul.holden@gmail.com>
2017-11-08 00:31:11 +0000

    Make sure UtilityCpp pulls in all libs it directly depends on

--- Source/Utility/ROMFileCache.cpp
+++ Source/Utility/ROMFileCache.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "stdafx.h"
-#include "ROMFileCache.h"
+#include "Utility/ROMFileCache.h"
 
 #include "Base/MathUtil.h"
 #include "Debug/DBGConsole.h"
