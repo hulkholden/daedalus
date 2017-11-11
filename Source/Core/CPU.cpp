f1ec5e0a0f325fbed52189dede542445f901cc3a
Paul Holden <paul.holden@gmail.com>
2017-11-11 15:28:16 +0000

    Move AtomicPrimitives to System

--- Source/Core/CPU.cpp
+++ Source/Core/CPU.cpp
@@ -44,10 +44,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Debug/DebugLog.h"
 #include "Debug/Synchroniser.h"
 #include "OSHLE/ultra_R4300.h"
+#include "System/AtomicPrimitives.h"
 #include "System/Mutex.h"
 #include "System/SystemInit.h"
 #include "System/Thread.h"
-#include "Utility/AtomicPrimitives.h"
 #include "Utility/Hash.h"
 
 #ifdef DAEDALUS_W32
