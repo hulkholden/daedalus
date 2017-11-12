b5a556fff1fcf9a430cfa24fd142c30b9902ee06
Paul Holden <paul.holden@gmail.com>
2017-11-12 12:42:56 +0000

    Move DaedalusW32.h to Platform/Windows

--- Source/stdafx.h
+++ Source/stdafx.h
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // Platform specifc #includes, externs, #defines etc
 #ifdef DAEDALUS_W32
-#include "DaedalusW32.h"
+#include "Platform/Windows/DaedalusW32.h"
 #endif
 
 #include "Base/Types.h"
