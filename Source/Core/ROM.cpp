120c2d56fdd4dc39ce4e12578ddb191303d787ce
Paul Holden <paul.holden@gmail.com>
2017-11-17 07:57:08 +0000

    Remove unnecessary header

--- Source/Core/ROM.cpp
+++ Source/Core/ROM.cpp
@@ -36,7 +36,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Debug/DebugLog.h"
 #include "Debug/Synchroniser.h"
 #include "HLEAudio/AudioPlugin.h"
-#include "HLEGraphics/GraphicsPlugin.h"
 #include "Interface/Preferences.h"
 #include "Interface/RomDB.h"
 #include "OSHLE/OSHLE.h"
