a9418cec70825b054e425b27ea2b345e8ab9d696
Paul Holden <paul.holden@gmail.com>
2017-12-09 16:03:48 +0000

    Fix runfiles support for Windows. Only the manifest is create - not the symlinks - so just parse this ourselves

--- Source/System/Paths.cpp
+++ Source/System/Paths.cpp
@@ -1,7 +1,11 @@
 #include "Base/Daedalus.h"
-#include "Paths.h"
+#include "System/Paths.h"
+
+#include <string>
+#include <unordered_map>
 
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
 
 #include "System/IO.h"
 
@@ -13,6 +17,9 @@ constexpr char kRunfilesDir[] = "/daedalus.exe.runfiles/";
 constexpr char kRunfilesDir[] = "/daedalus.runfiles/daedalus/";
 #endif
 
+static bool gHaveManifest = false;
+static std::unordered_map<std::string, std::string> gManifestMap;
+
 void SetExeFilename(const std::string& filename)
 {
 	std::string path = filename;
@@ -20,14 +27,41 @@ void SetExeFilename(const std::string& filename)
 	gDaedalusExePath = path;
 }
 
+// TODO(strmnnrmn): This doesn't work on Windows. Need to get the callers
+// to enumerate all the runfiles matching a glob, or something like that.
 std::string GetRunfilePath(absl::string_view filename)
 {
 	return IO::Path::Join(gDaedalusExePath, kRunfilesDir, filename);
 }
 
-bool LoadRunfile(absl::string_view filename, std::string* out)
+static bool _LoadManifest(std::unordered_map<std::string, std::string>* manifest_map)
 {
-	std::string fullpath = GetRunfilePath(filename);
+	std::string fullpath = GetRunfilePath("MANIFEST");
+	FILE * fh = fopen(fullpath.c_str(), "r");
+	if (!fh)
+	{
+		return false;
+	}
+
+	const u32 kBufferLen = 1024;
+	char buffer[kBufferLen + 1];
+	while (fgets(buffer, kBufferLen, fh))
+	{
+		absl::string_view line(buffer);
+		line = absl::StripTrailingAsciiWhitespace(line);
+		std::pair<std::string, std::string> kv = absl::StrSplit(line, absl::MaxSplits(' ', 1));
+		if (!kv.first.empty() && !kv.second.empty())
+		{
+			(*manifest_map)[kv.first] = kv.second;
+		}
+	}
+
+	fclose(fh);
+	return true;
+}
+
+static bool _LoadFile(const std::string& fullpath, std::string* out)
+{
 	FILE * fh = fopen(fullpath.c_str(), "rb");
 	if (!fh)
 	{
@@ -49,6 +83,21 @@ bool LoadRunfile(absl::string_view filename, std::string* out)
 	*out = p;
 	free(p);
 	return true;
+}
+
+bool LoadRunfile(absl::string_view filename, std::string* out)
+{
+	if (!gHaveManifest)
+	{
+		if (!_LoadManifest(&gManifestMap))
+		{
+			return false;
+		}
+		gHaveManifest = true;
+	}
+	std::string key = absl::StrCat("daedalus/", filename);
+	std::string fullpath = gManifestMap[key];
+	return _LoadFile(fullpath, out);
 }
 
 std::string GetDataFilename(absl::string_view filename)
